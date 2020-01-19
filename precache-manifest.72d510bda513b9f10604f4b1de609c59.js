d04087473ef6d8d0c26b5a7f015be4cc3fedcffc
Yasen <3622krim@gmail.com>
2020-01-19 12:07:24 -0800

    Updates

---
+++ precache-manifest.72d510bda513b9f10604f4b1de609c59.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83abe9442fe624c8dbe2a1eab30f2b4c",
+    "url": "/KUMIKO/index.html"
+  },
+  {
+    "revision": "f30ef22b6686ca365652",
+    "url": "/KUMIKO/static/css/2.ef122f89.chunk.css"
+  },
+  {
+    "revision": "f30ef22b6686ca365652",
+    "url": "/KUMIKO/static/js/2.cc6fa6db.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/KUMIKO/static/js/2.cc6fa6db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcfffb9c52f9e6cb8663",
+    "url": "/KUMIKO/static/js/main.32a22157.chunk.js"
+  },
+  {
+    "revision": "54e850f7dcf575774cee",
+    "url": "/KUMIKO/static/js/runtime-main.1e2efe92.js"
+  },
+  {
+    "revision": "379a3cbfd782de0c1a9ff77b4c25ee78",
+    "url": "/KUMIKO/static/media/appetizer.379a3cbf.svg"
+  },
+  {
+    "revision": "4e2d8359e6f4bec5dea04cce4b6f7a97",
+    "url": "/KUMIKO/static/media/chef.4e2d8359.svg"
+  },
+  {
+    "revision": "bd9d70701efec25ca6aeac0e6ecc65c8",
+    "url": "/KUMIKO/static/media/chopsticks.bd9d7070.svg"
+  },
+  {
+    "revision": "c6c3a4ce4522a820880dc569f408a719",
+    "url": "/KUMIKO/static/media/eat.c6c3a4ce.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/KUMIKO/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/KUMIKO/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/KUMIKO/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/KUMIKO/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/KUMIKO/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "18bb77256521f67631662692a73a613b",
+    "url": "/KUMIKO/static/media/food-and-restaurant (1).18bb7725.svg"
+  },
+  {
+    "revision": "3289fba7d29a47bc083da14f0c670962",
+    "url": "/KUMIKO/static/media/fried-rice.3289fba7.svg"
+  },
+  {
+    "revision": "bb677c03c8b65debe8fff812fffc3fe0",
+    "url": "/KUMIKO/static/media/noodles.bb677c03.svg"
+  },
+  {
+    "revision": "dee3f13125510c0a80e9920d419c57fb",
+    "url": "/KUMIKO/static/media/pot.dee3f131.svg"
+  },
+  {
+    "revision": "8d14369cdacafa8214e6aad726458728",
+    "url": "/KUMIKO/static/media/salad.8d14369c.svg"
+  },
+  {
+    "revision": "ae7a25f339f88b3a1cbc0af35c7a58cb",
+    "url": "/KUMIKO/static/media/sashimi.ae7a25f3.svg"
+  },
+  {
+    "revision": "7591899ce8c56e53f0e3385b1248b6d0",
+    "url": "/KUMIKO/static/media/sushi3.7591899c.svg"
+  },
+  {
+    "revision": "b596f6dcdc3fb74c18116cb71d802f35",
+    "url": "/KUMIKO/static/media/sushi_1.b596f6dc.svg"
+  },
+  {
+    "revision": "67a9fa400d60bfaeb0a986b9eadccce2",
+    "url": "/KUMIKO/static/media/sushi_2.67a9fa40.svg"
+  },
+  {
+    "revision": "e7b68e5b9dc1de697ecd1f865af06acc",
+    "url": "/KUMIKO/static/media/sushi_com.e7b68e5b.svg"
+  },
+  {
+    "revision": "f0b58e60bab11d517378ec6c3b191317",
+    "url": "/KUMIKO/static/media/tapas.f0b58e60.svg"
+  }
+]);
